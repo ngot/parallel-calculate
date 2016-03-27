5927fbd1ffec71dfea9beb777e5fb2377d4b41b8
ngot <zhuanghengfei@gmail.com>
2016-03-27 11:12:51 +0800

    feat:more fast benchmark

--- benchmark/test.js
+++ benchmark/test.js
@@ -5,7 +5,7 @@ const os = require('os');
 const workPath = path.join(__dirname, '../test/support/worker.js');
 
 let oneThread = new Calculate(workPath, 1);
-let mutiThread = new Calculate(workPath);
+let mutiThread = new Calculate(workPath, 16);
 
 // large data to cal
 let arrs = [];
@@ -13,12 +13,12 @@ for (let i = 0; i < 5000000; i++) {
   arrs.push(i);
 }
 
-console.notice(`cal start with ${os.CPUs()} thread ...`);
+console.notice(`cal start with 16 fibers ...`);
 console.time('mutiThread');
 mutiThread.proxy(arrs, 'plus')
 console.timeEnd('mutiThread');
 
-console.notice(`cal start with 1 thread ...`);
+console.notice(`cal start with 1 fibers ...`);
 console.time('oneThread');
 oneThread.proxy(arrs, 'plus')
 console.timeEnd('oneThread');
